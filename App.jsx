1aab43f072437f8e1b4a4267c1273e4e557faa37
riya-k003 <161316727+riya-k003@users.noreply.github.com>
2025-03-23 20:45:59 +0530

    Update App.jsx removed unnecessary code lines

--- App.jsx
+++ App.jsx
@@ -1,20 +1,10 @@
-// import MyComponent from './mycomponent.jsx'
-// import Counter from './counter.jsx'
-// import ColorPicker from './colorpicked.jsx'
-// import Update from './update.jsx'
-// import Array from './array.jsx'
-// import ArrayObject from './array_object.jsx'
+
 import ToDoList from './ToDoLIST.jsx'
 
 function App() {
   return(
     <>
-   {/* <MyComponent/> */}
-   {/* <Counter/> */}
-   {/* <ColorPicker/> */}
-   {/* <Update/> */}
-   {/* <Array/> */}
-   {/* <ArrayObject/> */}
+
    <ToDoList/>
     </>
   ); 
